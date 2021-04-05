c8d1de3d0d533a1ff2115e3fcf24b502c771e5d1
Muertech <hello@muertech.com>
2021-04-05 03:46:21 -0700

    Runs and refactored

--- project_euler_3.py
+++ project_euler_3.py
@@ -1,34 +1,17 @@
-# DOES NOT RUN CURRENTLY, WORKED FOR SMALL NUMBERS ONLY
-# import math
-#
-#
-# end_list = []
-#
-#
-# def sieve(target):
-#     prime_list = [True] * (target + 1)
-#     prime_list[0] = prime_list[1] = False
-#
-#     for i, prime_check in enumerate(prime_list):
-#         if prime_check:
-#             yield i
-#             for j in range(i*i, target, i):
-#                 prime_list[j] = False
-#
-#     for i in range(0, target):
-#         if prime_list[i]:
-#             end_list.append(i)
-#
-#
-# sieve(970)
-# end_list.reverse()
-# print(end_list[0])
-#
-# def largest_prime(n):
-#     value = 0
-#     for x in range(0, len(end_list)):
-#         if n % end_list[x] == 0:
-#             print(end_list[x])
-#             break
-#
-# largestprime(970)
+# Find the largest prime factor of n, n = 600851475143
+
+
+import math
+
+
+def largest_prime(n):
+    for x in range(2, int(math.sqrt(n)) + 1):
+        while n % x == 0:
+            n = n / x
+            if n == 1 or n == x:
+                print(x)
+                break
+
+
+largest_prime(600851475143)
+
