7bc0c4c26523d2fb92d9330950ff2a89f1f5570b
Muertech <hello@muertech.com>
2021-04-22 19:40:28 -0700

    Refactored a few, completed incomplete exercises

---
+++ euler_5.py
@@ -0,0 +1,23 @@
+# Find smallest number divisible by all numbers 1-20
+# Naive semi-optimized brute force math solution:
+# Take 2520 (smallest number divisible by all numbers - 10
+# Take all prime numbers from 11 - 20: 11, 13, 17, 19
+# Multiply 2520 x 11 x 13 x 17 x 19 = 116396280 to get higher starting number for bruteforce
+# Note that the starting number is divisible by 20, so we can increment +20
+
+
+def divisibility_check(number):
+    for y in range(1, 20):
+        if number % y != 0:
+            return False
+    return True
+
+
+def largest_divisible():
+    x = 116396280
+    while not divisibility_check(x):
+        x += 20
+    return x
+
+
+print(largest_divisible())
