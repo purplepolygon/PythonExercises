d098a1139676ab22d04248e130e680e6881b7e5a
PurplePolygon <hello@muertech.com>
2021-08-26 04:32:10 -0700

    Finished is_unique with dict hashmap lookup

--- CTCI/Arrays_Strings/Permutations/is_unique.py
+++ CTCI/Arrays_Strings/Permutations/is_unique.py
@@ -2,34 +2,19 @@
 
 
 # Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
-# cannot use additional data structures? O(n^2) Brute Force Solution
+# cannot use additional data structures?
 
 
 def is_unique(unique_string):
-    string_list = [x for x in unique_string]
-    i = 0
-    j = 0
-    while j <= len(string_list):
-        for i in range(len(string_list) - 1):
-            j = i + 1
-            print(len(string_list))
-            print(str(i) + "step2")
-            print(str(j) + "step1")
-            for j in range(1, len(string_list)):
-                print("check")
-                print(i)
-                print(j)
-                if string_list[i] == string_list[j]:
-                    print(string_list[i], string_list[j])
-                    print(i, j)
-                    return False
-                else:
-                    j += 1
-                    continue
-                i += 1
-        return True
+    string_list = [x.lower() for x in unique_string]
+    string_dict = {}
+    for x in string_list:
+        if x in string_dict:
+            return False
+        else:
+            string_dict[x] = "1"
+            continue
+    return True
 
 
-print(is_unique("comptn"))
-
-# (Get rid of J loop, just use while loop + for loop to solve this )
+print(is_unique("compCwtn"))
